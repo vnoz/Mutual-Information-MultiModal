60e9708aa7d60793a7d48e92639bc8ecf44bb2fb
Tuan laptop <naut1224@gmail.com>
2024-09-30 20:53:48 +0800

    Add same transform fn to CXRImageDataset as in CXRImageReport

--- mutual_info_img_txt/main_utils.py
+++ mutual_info_img_txt/main_utils.py
@@ -257,9 +257,9 @@ class ExplainableImageModelManager:
 		Create an instance of traning data loader
 		'''
 
-		# random_degrees = [-20,20]
-		# random_translate = [0.1,0.1]
-		# img_size = args.img_size
+		random_degrees = [-20,20]
+		random_translate = [0.1,0.1]
+		img_size = args.img_size
 		
 		# transform =transforms.Compose([
 		# 	# transforms.Resize(256),
@@ -279,14 +279,22 @@ class ExplainableImageModelManager:
 		# 	# transforms.Resize(128)
 		# ])
 
-		transform =transforms.Compose([pre_trained_img_model])
+		transform = torchvision.transforms.Compose([
+		torchvision.transforms.Lambda(lambda img: img.astype(np.int16)),
+		torchvision.transforms.ToPILImage(),
+		torchvision.transforms.RandomAffine(degrees=random_degrees, translate=random_translate),
+		torchvision.transforms.CenterCrop(img_size),
+		torchvision.transforms.Lambda(
+			lambda img: np.array(img).astype(np.float32)),
+		torchvision.transforms.Lambda(
+			lambda img: img / max(1e-3, img.max()))
+		])
+
 		
 		dataset = CXRImageDataset(img_dir=args.image_dir, 
 									dataset_metadata=args.dataset_metadata, 
 									disease='Pleural Effusion',
 									transform=transform)
-									# transform=None)
-									# transform=pre_trained_img_model)
 		
 		data_loader = torch.utils.data.DataLoader(dataset=dataset,
                                           batch_size=1,
@@ -306,11 +314,10 @@ class ExplainableImageModelManager:
     		
 			avg_cost = 0
 
-			for image_embeddings, label in data_loader:
-				# image_embeddings = pre_trained_img_model(image)
-				# image_embeddings = image_embeddings.to(device)
-				# image = image.to(device)
+			for image, label in data_loader:
+				image_embeddings = pre_trained_img_model(image)
 				image_embeddings= image_embeddings.to(device)
+				
 				label = label.to(device)
 
 				optimizer.zero_grad()
