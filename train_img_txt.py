350b02fcf8e37c82c7dbf481e1f79674622cc3b5
Tuan laptop <naut1224@gmail.com>
2024-09-30 18:50:37 +0800

    Fix ctor in train_image_classifier

--- train_img_txt.py
+++ train_img_txt.py
@@ -134,7 +134,7 @@ def train_image_classifier():
 
     model_manager = ExplainableImageModelManager()
 
-    model_manager.train( pre_trained_img_model=load_pre_trained_model() , device=device,
+    model_manager.train( pre_trained_img_model=load_pre_trained_model , device=device,
                         args=args)
     
 train_image_classifier()
