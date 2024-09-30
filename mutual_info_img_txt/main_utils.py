aed3e3daa01bc7bbcfd41fba3f7bceb7421b13d3
Tuan laptop <naut1224@gmail.com>
2024-09-30 22:35:14 +0800

    move fn load_pre_trained_model inside ExplainableImageModelManager

--- mutual_info_img_txt/main_utils.py
+++ mutual_info_img_txt/main_utils.py
@@ -274,7 +274,17 @@ class ExplainableImageModelManager:
 			lambda img: img / max(1e-3, img.max()))
 		])
 
+		#NOTE: Load pre_trained image model from MI training
+		save_dir = os.path.join(args.save_dir,
+                                 f'{args.mi_estimator}_total_epochs{args.num_train_epochs}')
+		output_model_file = os.path.join(save_dir, 
+													'pytorch_model_epoch'+str(args.num_train_epochs)+'.bin')
+
+		self.pre_trained_img_model = build_resnet_model(model_name=args.image_model_name, checkpoint_path=output_model_file,
+													output_channels=args.output_channels)
 		
+
+
 		dataset = CXRImageDataset(img_dir=args.image_dir, 
 									dataset_metadata=args.dataset_metadata, 
 									disease='Pleural Effusion',
@@ -288,8 +298,7 @@ class ExplainableImageModelManager:
 		Define Loss function and optimizer
 		'''
 		self.image_classifier_model = self.image_classifier_model.to(device)
-		self.pre_trained_img_model = load_pre_trained_model()
-
+		
 		criterion = torch.nn.CrossEntropyLoss().to(device)    # Softmax is internally computed.
 		optimizer = torch.optim.Adam(self.image_classifier_model.parameters(), lr=args.init_lr)
 
