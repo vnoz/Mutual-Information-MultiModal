614feda73da2db30c0da19073c1e81fd3011c2ca
Tuan laptop <naut1224@gmail.com>
2024-10-08 02:28:11 +0800

    Update args to jpg training

--- mutual_info_img_txt/model_utils.py
+++ mutual_info_img_txt/model_utils.py
@@ -91,7 +91,7 @@ class CXRImageReportDataset(torchvision.datasets.VisionDataset):
         if self.cache_images:
             img = self.images[str(idx)]
         else:
-            png_path = os.path.join(self.img_dir, f'{img_id}.png')
+            png_path = os.path.join(self.img_dir, f'{img_id}.jpg')
             img = cv2.imread(png_path, cv2.IMREAD_ANYDEPTH)
 
         if self.transform is not None:
@@ -104,7 +104,7 @@ class CXRImageReportDataset(torchvision.datasets.VisionDataset):
     def cache_img_set(self):
         for idx in range(self.__len__()):
             img_id = self.dataset_metadata.loc[idx, self.data_key]
-            png_path = os.path.join(self.data_dir, f'{img_id}.png')
+            png_path = os.path.join(self.data_dir, f'{img_id}.jpg')
             img = cv2.imread(png_path, cv2.IMREAD_ANYDEPTH)
             if idx == 0:
                 self.images = {}
