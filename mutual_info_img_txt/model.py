cf7419a855e00897b40adde1250665f31ed48ee9
Tuan laptop <naut1224@gmail.com>
2024-10-08 01:55:50 +0800

    do not use sigmoid in Basic_MLP

--- mutual_info_img_txt/model.py
+++ mutual_info_img_txt/model.py
@@ -160,7 +160,7 @@ class Basic_MLP(nn.Module):
         x = self.relu(x)
         # x = self.softmax(x)
         x = x.view(x.size(0), -1)
-        x = self.sigmoid(x)
+       # x = self.sigmoid(x)
         # y = self.softmax(y_logits)
         return x
     
