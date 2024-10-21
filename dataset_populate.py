2c2a22420c45560a7a3366ebe4ebb28dd71ac2ab
Tuan laptop <naut1224@gmail.com>
2024-10-21 23:54:51 +0800

    Fix parameters issue for csv parser

--- dataset_populate.py
+++ dataset_populate.py
@@ -307,7 +307,7 @@ def populate_subset_dataset(amount, image_dir,text_dir, metadata):
 def parsing_csv_meta_data_for_label_stats(metadata):
     result = {}
 
-    with open(os.path.join(args.data_dir,metadata), 'rt') as csvfile:
+    with open(metadata, 'rt') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
         line_count=0
         labels=[]
@@ -323,7 +323,7 @@ def parsing_csv_meta_data_for_label_stats(metadata):
 
     print(result)
 
-    with open(os.path.join(args.data_dir,'disease_stats'), 'w') as tsv_file:
+    with open(os.path.join(args.training_data_dir,'disease_stats.csv'), 'w') as tsv_file:
         tsv_writer = csv.writer(tsv_file)
         tsv_writer.writerows(result)
 
