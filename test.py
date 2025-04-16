17aa8bd172b502c9bb16f00379586a2eeebd182f
Tuan laptop <naut1224@gmail.com>
2025-04-17 00:16:41 +0800

    Add Encoder Resnet50 for test

--- test.py
+++ test.py
@@ -1,29 +1,62 @@
-import os
-import cv2 # type: ignore
-from mutual_info_img_txt.model import Basic_MLP
-from mutual_info_img_txt.model_utils import generate_GradCAM_image
-import torch  # type: ignore
-from helpers import get_transform_function
+#https://github.com/arnaghosh/Auto-Encoder/blob/master/resnet.py
 
-from pytorch_grad_cam import GradCAM, HiResCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM, EigenCAM, FullGrad # type: ignore
-from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget # type: ignore
-from pytorch_grad_cam.utils.image import show_cam_on_image # type: ignore
+class Encoder(nn.Module):
 
-img_path = os.path.join('full_data_set/images/p10', 'p10000032_s50414267_02aa804e-bde0afdd-112c0b34-7bc16630-4e384014.jpg')
+    def __init__(self, block, layers, num_classes=23):
+        self.inplanes = 64
+        super (Encoder, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)#, return_indices = True)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512 * block.expansion, 1000)
+	#self.fc = nn.Linear(num_classes,16) 
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
 
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
 
-print('Start generate heatmap for image: ' + str(img_path))
+        return nn.Sequential(*layers)
 
-img = cv2.imread(img_path, cv2.IMREAD_ANYDEPTH)
-transform_fn = get_transform_function(256)
-img = transform_fn(img)
+    def forward(self, x):
+        x = self.conv1(x)
+	
+        x = self.bn1(x)
+        x = self.relu(x)
+	
+        x = self.maxpool(x)
+	
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
 
-output_model_file = os.path.join('save_dir/dv_total_epochs10', 'pytorch_image_classifier_Edema.bin')
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
 
-image_classifier_model = Basic_MLP.load_from_pretrained(output_model_file)
-
-generate_GradCAM_image(image_classifier_model, device=device, input_image = img,location_path='save_dir/heatmap')
-
-print('Finish generate heatmap for image: ' + str(img_path))
+        return x
